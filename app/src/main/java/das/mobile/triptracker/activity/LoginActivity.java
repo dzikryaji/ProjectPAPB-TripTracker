de2c1d592615eb16692b6e8d0e2551135b7ff455
MJ-Avers <averroes0309@gmail.com>
2023-11-26 22:12:10 +0700

    login & register

---
+++ app/src/main/java/das/mobile/triptracker/activity/LoginActivity.java
@@ -0,0 +1,16 @@
+package das.mobile.triptracker.activity;
+
+import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import das.mobile.triptracker.R;
+
+public class LoginActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+    }
+}
