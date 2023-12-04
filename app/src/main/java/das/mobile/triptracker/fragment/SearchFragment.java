910025c704371591a0b7c1c3b58893f211698996
dzikryaji <dzikryaji07@gmail.com>
2023-12-04 22:44:43 +0700

    search

--- app/src/main/java/das/mobile/triptracker/fragment/SearchFragment.java
+++ app/src/main/java/das/mobile/triptracker/fragment/SearchFragment.java
@@ -1,9 +1,12 @@
 package das.mobile.triptracker.fragment;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.view.inputmethod.EditorInfo;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
@@ -27,6 +30,7 @@ public class SearchFragment extends Fragment {
     PostAdapter postAdapter;
     DatabaseReference db = FirebaseDatabase.getInstance().getReference();
     ArrayList<Post> listPost;
+    FragmentSearchBinding binding;
 
     public SearchFragment(FirebaseUser currentUser) {
         this.currentUser = currentUser;
@@ -41,7 +45,7 @@ public class SearchFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        FragmentSearchBinding binding = FragmentSearchBinding.inflate(inflater, container, false);
+        binding = FragmentSearchBinding.inflate(inflater, container, false);
         binding.rvSearch.setLayoutManager(new LinearLayoutManager(getActivity()));
         db.child("posts").addValueEventListener(new ValueEventListener() {
             @Override
@@ -61,6 +65,43 @@ public class SearchFragment extends Fragment {
 
             }
         });
+
+        binding.etSearch.setOnEditorActionListener(new TextView.OnEditorActionListener() {
+            @Override
+            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
+                if (actionId == EditorInfo.IME_ACTION_SEARCH) {
+                    String query = binding.etSearch.getText().toString();
+                    performSearch(query);
+                    return true;
+                }
+                return false;
+            }
+        });
+
         return binding.getRoot();
+    }
+
+    private void performSearch(String searchQuery) {
+        db.child("posts").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                listPost = new ArrayList<>();
+                for (DataSnapshot item : snapshot.getChildren()) {
+                    Post post = item.getValue(Post.class);
+                    post.setId(item.getKey());
+                    String text = post.getText();
+                    if (text.contains(searchQuery)){
+                        listPost.add(post);
+                    }
+                }
+                postAdapter = new PostAdapter(listPost, getActivity(), currentUser);
+                binding.rvSearch.setAdapter(postAdapter);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 }
